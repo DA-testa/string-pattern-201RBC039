74f256074b0d0b783b7e666bce395f170e6ab0d1
201RBC039 <125255543+201RBC039@users.noreply.github.com>
2023-04-17 13:02:03 +0300

    Update hash_substring.py
    result 1

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,46 @@
 # python3
 
+B=17
+Q=256
+
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    actionChoose = input()
+    if  "i" in actionChoose.lower():        
+        patternInput = input()
+        textData = input()
+
+    elif "f" in actionChoose.lower():
+        name = "./tests/06"
+        with open(name, mode = "r") as f:
+            patternInput = f.readline()
+            textData = f.readline()
+    return (patternInput.rstrip(), textData.rstrip())
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    man =[]
+    patternLn = len(pattern)
+    hash_pattern_res = get_hash(patternLn)
+    textLn = len(text)
+    for j in range(0, textLn-patternLn+1):
+        hash_text_res = get_hash(text[j:j+patternLn])
+        if hash_pattern_res == hash_text_res:
+            if pattern==text[j:j+patternLn]:
+                man.append(j)
 
-    # and return an iterable variable
-    return [0]
+    return man
 
 
-# this part launches the functions
+def get_hash(pattern)->int:
+    global B,Q
+    res =0
+    patternLength = len(pattern)
+    for i in range(patternLength):
+        res =(B*res+ord(pattern[i]))%Q
+    return res
+    
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
 
